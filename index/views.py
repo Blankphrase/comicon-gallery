a9ec8936f71293f1140f689c6384f10458cf2ec9
jack ogina <jackogina60@gmail.com>
2018-06-15 13:43:05 -0400

    created search view
    Handles search requests
    Users input is used to search for images that much that description using inbuilt regex search
    renders the results with the following params: found photos, locations, categories
    template: search.html

--- index/views.py
+++ index/views.py
@@ -18,4 +18,26 @@ def index(request):
                    "locations":locations,
                    "categories":categories})
 
+def search_results(request):
+    '''
+    Handles search requests
+    Users input is used to search for images that much that description using inbuilt regex search
+    renders the results with the following params: found photos, locations, categories
+    '''
+    if 'image' in request.GET and request.GET["image"]:
+        query = request.GET.get("image")
+        photos = Image.searched(query)
+        message = f"{query}"
+
+        return render(request, 'results.html',
+                      {"message": message, "photos": photos,"locations":locations,"categories":categories})
+    else:
+        message = "What images do you want to search for?"
+        return render(request, 'results.html',
+                      {"message": message,"locations":locations,"categories":categories})
+
+
+
+
+
 
