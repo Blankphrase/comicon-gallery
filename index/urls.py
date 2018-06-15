6be27fb3f0eee4920d32b87b0402debaed35f04d
jack ogina <jackogina60@gmail.com>
2018-06-15 13:26:57 -0400

    MEDIA url settings
    added media document root path to enable app to display photos

--- index/urls.py
+++ index/urls.py
@@ -16,7 +16,9 @@ urlpatterns = [
     path('', views.index, name='welcome'),
     path('search', views.search_results, name='search_results'),
    ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 
 
