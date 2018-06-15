d70292ae52d9387b53799336f4c524421d264509
jack ogina <jackogina60@gmail.com>
2018-06-15 13:37:58 -0400

    created the Image model
    image FileField, image url Char field, image_name charfield, description textfield, category M2M field, post_date datefield and location FK field
    save, delete and update methods
    get_all method fro retrieving all images from the db
    get_image by id method
    search - get image by description method
    today_image - get latest image
    filter_methods - filter by location or by category

--- index/models.py
+++ index/models.py
@@ -56,6 +56,65 @@ class Category(models.Model):
         return self.name
 
 
+class Image(models.Model):
+    '''
+    Image model
+    '''
+    image = models.ImageField(upload_to='gallery/', blank=True)
+    image_url = models.TextField(blank=False)
+    image_name = models.CharField(max_length=30, blank=False)
+    description = models.TextField(max_length=100, blank=False)
+    category = models.ManyToManyField('Category',blank=True)
+    post_date = models.DateTimeField(auto_now=True)
+    location = models.ForeignKey('Location', on_delete=models.CASCADE,blank=True,null=True)
 
+    class Meta:
+        ordering = ['-post_date']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls, id, target, update):
+        updated = cls.objects.filter(id=id).update(target=update)
+        return updated
+
+    @classmethod
+    def get_all(cls):
+        images = cls.objects.order_by('-post_date')
+        return images
+
+    @classmethod
+    def get_image(cls, id):
+        image = cls.objects.get(id=id)
+        return image
+
+    @classmethod
+    def searched(cls, query):
+        result = cls.objects.filter(
+            description__icontains=query).order_by('-post_date')
+        return result
+
+    @classmethod
+    def today_pics(cls):
+        today = dt.date.today()
+        images = cls.objects.filter(post_date__date=today)
+        return images
+
+    @classmethod
+    def filter_location(cls,place):
+        images = cls.objects.filter(
+            location__location__startswith=place).order_by('-post_date')
+        return images
+
+
+    @classmethod
+    def filter_category(cls,query):
+        images = cls.objects.filter(
+            category__name__startswith=query).order_by('-post_date')
+        return images
 
 
