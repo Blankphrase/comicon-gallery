de4f45074e11dc8292541948b6d363f9ea94277c
jack ogina <jackogina60@gmail.com>
2018-06-15 14:00:18 -0400

    created test for view: Search
    tested if views url existed at desired location
    tested if view url can be accessible by name
    tested if view uses the correct template

--- index/tests/test_views.py
+++ index/tests/test_views.py
@@ -28,5 +28,30 @@ class TestIndexView(TestCase):
 
         self.assertTemplateUsed(resp, self.template_name)
 
+class TestSearchView(TestCase):
+    '''
+    test if view url exists at desired location
+    test if view url accessible by name
+    test if view uses correct template
+    '''
+    def setUp(self):
+        self.view_name="search_results"
+        self.route="/search?image=iron"
+        self.template_name="results.html"
+
+    def test_view_url_exists_at_desired_location(self): 
+        resp = self.client.get(self.route) 
+        self.assertEqual(resp.status_code, 200)  
+           
+    def test_view_url_accessible_by_name(self):
+        resp = self.client.get(reverse(self.view_name))
+        self.assertEqual(resp.status_code, 200)
+        
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse(self.view_name))
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertTemplateUsed(resp, self.template_name)
+
 
 
